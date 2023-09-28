eaf1fdd2043f2a015ec1ba8b81c2e2ff37b911a7
Khead2 <ha149366@gmail.com>
2023-09-28 11:51:21 +0700

    menambah alert

--- tugas4php/tugas4php.php
+++ tugas4php/tugas4php.php
@@ -4,17 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
         }
-        .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh; 
-    }
+        
     th{
        padding-bottom:30px;
     }
@@ -24,25 +19,54 @@
         font-weight: bold;
         font-size: 13px;
     }
-    #button{
+    /* #button{
        padding-top: 10px;
-    }
+    } */
     .freme{
         background-color: #C4DFDF;
         border-radius: 10px;
         padding: 15px;
+        width: 50%;
+        
+    }
+    .alert{
+        width: 50%;
     }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="freme">
+<?php 
+if(isset($_POST['proses'])){
+    $username = $_POST['username'];
+    $password = $_POST['pass'];
 
-        <form method= "POST">
+
+    function ceklogin($username, $password){
+        if ($username == "admin" && $password == "mimin"){
+            header("Location: admin.php");
+                exit();
+        }else {
+            echo '
+            <div class="container d-flex justify-content-center mt-2">
+            <div class="alert alert-warning alert-dismissible ">Username atau password anda salah 
+            <button type="button" class="btn-close btn-close-black " data-bs-dismiss="alert" aria-label="Close">
+            </button>
+            </div>
+            </div>';
+            
+        }
+    }
+   ceklogin($username,$password);
+}
+?>
+    <div class="container d-flex justify-content-center mt-5 ">
+        <div class="freme justify-content-center align-item-center" >
+
+        <form class="d-flex justify-content-center" method= "POST">
             <table>
                 <thead>
                     <tr>
-                   <th colspan="2" align="center">login cuy</th>
+                   <th colspan="2" class="text-center">login cuy</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -72,31 +96,10 @@
                 </tfoot>
             </table>
         </form>
-        <?php 
- 
-
-// $tombol = $_POST['proses'];
-
-  
-
-if(isset($_POST['proses'])){
-    $username = $_POST['username'];
-    $password = $_POST['pass'];
-
-
-    function ceklogin($username, $password){
-        if ($username == "admin" && $password == "mimin"){
-            header("Location: admin.php");
-                exit();
-        }else {
-            echo "Login gagal. Silakan coba lagi.";
-        }
-    }
-   ceklogin($username,$password);
-}
-?>
         </div>
     </div>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
 
